e07669ad7a73da07b819d2289776e9538c546e4b
rustem <rkh01106@gmail.com>
2023-03-27 09:27:50 +0300

    Deleted unnecessary files

--- jb.py
+++
@@ -1,8 +0,0 @@
-1.
-
-df3 = pd.merge(df1, df2, type = inner, on = ['date', 'acc'])
-
-df3['Маржа'] = df3['revenue'] / df3['balance']
-
-df4 = df3.group_by(['date', 'type', 'cur'])['balance', 'revenue'].sum()
-df4['Маржа'] = df4['revenue'] / df4['balance']
